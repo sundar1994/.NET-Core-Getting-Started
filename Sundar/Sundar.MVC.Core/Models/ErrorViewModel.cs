1f51f7d94f474c7140b4b55d026ca21ca2981dd2
Sundar <85296392@qq.com>
2018-11-29 10:47:45 +0800

    初始项目

---
+++ Sundar/Sundar.MVC.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sundar.MVC.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
